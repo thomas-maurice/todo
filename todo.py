f031429bc6987464befcc26e8134c29ac9fd7279
thomas <tmaurice59@gmail.com>
2014-01-20 09:26:54 +0100

    Added license info

--- todo.py
+++ todo.py
@@ -1,6 +1,33 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+"""
+	  pypodcatcher.py : A simple podcast client commandline software
+	
+	           DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+                   Version 2, December 2004
+ 
+	Copyright (C) 2013 Thomas Maurice <tmaurice59@gmail.com>
+	 
+	Everyone is permitted to copy and distribute verbatim or modified
+	copies of this license document, and changing it is allowed as long
+	as the name is changed.
+	 
+		         DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+		TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION
+	 
+	 0. You just DO WHAT THE FUCK YOU WANT TO.
+	 
+"""
+
+__author__ = "Thomas Maurice"
+__copyright__ = "Copyright 2014, Thomas Maurice"
+__license__ = "WTFPL"
+__version__ = "0.2"
+__maintainer__ = "Thomas Maurice"
+__email__ = "tmaurice59@gmail.com"
+__status__ = "Development"
+
 import xml.dom.minidom as minidom
 import sys
 import os
