f9390476e72c8691c6cd2494a1bc980b0cb09199
thomas <tmaurice59@gmail.com>
2014-01-20 09:35:16 +0100

    Added a lot of documentation

--- todo.py
+++ todo.py
@@ -51,6 +51,9 @@ class TodoList:
 		 - self.todo_list: The xml nodelist stripped from the blank one
 	"""
 	def __init__(self, todo_file):
+		"""
+			Initializes a TodoList obect from an XML file
+		"""
 		self.todo_file_name = todo_file
 		try:
 			self.todo_file = minidom.parse(todo_file)
@@ -63,22 +66,40 @@ class TodoList:
 			self.regenerate_todo_list()
 	
 	def regenerate_todo_list(self):
+		"""
+			Regenerates the self.todo_list variable
+			
+			This will strip out the blank nodes of
+			the main xml nodelist. Must be called
+			after each addition/deletion of members
+		"""
 		self.todo_list = []
 		for e in self.todo_file.firstChild.childNodes:
 			if e.nodeType == e.ELEMENT_NODE:
 				self.todo_list.append(e)
 		
 	def save(self):
+		"""
+			Saves the todolist
+			
+			Saves the self.todo_file to a file
+		"""
 		f = open(self.todo_file_name, "w")
 		f.write(self.todo_file.toprettyxml().replace("\t", "").replace("\n\n", "").encode("utf-8"))
 		f.close()
 	
 	def create_new_file(self, file_name):
+		"""
+			Creates a new bare file containing nothing to do
+		"""
 		f = open(self.todo_file_name, "w")
 		f.write("<?xml version=\"0.1\" encoding=\"UTF-8\" ?><todolist></todolist>")
 		f.close()
 	
 	def remove_todo_by_id(self, i):
+		"""
+			Removes a node in the self.todo_file by its ID 
+		"""
 		for t in self.todo_list:
 			if t.nodeType == t.ELEMENT_NODE:
 				if int(t.attributes["id"].value) == int(i):
@@ -86,6 +107,12 @@ class TodoList:
 					return
 	
 	def set_priority(self, i, p=""):
+		"""
+			Changes the priority of a todo
+			
+			NOT IMPLEMENTED YET, the priority still has no effect on
+			nothing. Will be implemented soon.
+		"""
 		for e in self.todo_file.firstChild.childNodes:
 			if e.nodeType == e.ELEMENT_NODE:
 				if int(e.attributes["id"].value) == int(i):
@@ -93,6 +120,9 @@ class TodoList:
 					return
 	
 	def add_todo(self, content):
+		"""
+			Creates a new Todo containing the "content" content
+		"""
 		i = self.get_max_id()+1
 		e = self.todo_file.createElement("todo")
 		e.attributes["id"] = str(i)
@@ -105,9 +135,17 @@ class TodoList:
 		self.todo_file.firstChild.appendChild(e)
 	
 	def print_todo(self, todo):
+		"""
+			Prints a todo with pretty colors <3
+		"""
 		print colored(" #%2d" % int(todo.attributes["id"].value), "blue"), ":", self.colorize_todo(todo.attributes["task"].value)
 		
 	def get_todo_by_id(self, i, todo_list=None):
+		"""
+			Returns the XML node corresponding to the todo with the id "i"
+			within the given todo_list. If None, then the one used by
+			the class shall be used. This is the case most of the time
+		"""
 		if todo_list == None:
 			todo_list = self.todo_list
 			
@@ -118,23 +156,38 @@ class TodoList:
 		return None
 	
 	def get_todos_by_tag(self, tag, todo_list=None):
+		"""
+			Unimplemented yet
+		"""
 		if todo_list == None:
 			todo_list = self.todo_list
 	
 	def sort_todos_by_id(self, todo_list=None):
+		"""
+			Sorts the todo list by ID
+		"""
 		if todo_list == None:
 			todo_list = self.todo_list
 			
 		return sorted(todo_list, key=lambda k: k.attributes["id"].value)
 	
 	def count_todos(self):
+		"""
+			Returns the number of todos currently registered
+		"""
 		return len(self.todo_list)
 	
 	def print_all(self):
+		"""
+			Print all the todos registered
+		"""
 		for todo in self.todo_list:
 			self.print_todo(todo)
 	
 	def colorize_todo(self, text):
+		"""
+			Colorizes a todo, this means setting a pretty color on hashtags
+		"""
 		text = text.split(" ")
 		for i in range(0, len(text)):
 			if text[i][0] == '#':
@@ -143,6 +196,9 @@ class TodoList:
 				
 	
 	def print_todos(self, todo_list=None):
+		"""
+			Prints all the todos within a list
+		"""
 		if todo_list == None:
 			todo_list = self.todo_list
 		
@@ -150,6 +206,9 @@ class TodoList:
 			self.print_todo(t)
 	
 	def get_max_id(self):
+		"""
+			Returns the maximum ID within the todo list
+		"""
 		m = 0
 		for e in self.todo_list:
 			if int(e.attributes["id"].value) > m:
