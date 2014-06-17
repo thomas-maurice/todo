c8607481344948a86ab68250dd418896ab6e0151
thomas <tmaurice59@gmail.com>
2014-06-17 10:10:13 +0200

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+setup(
+    name="todo.py",
+    version="0.1",
+    author="Thomas Maurice",
+    url="https://github.com/svartbergtroll/todo",
+    scripts = ['todo.py']
+)
